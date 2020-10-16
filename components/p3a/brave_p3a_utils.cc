cdda6b46f80e1d480edf0a76b48255f3a1d8435d
Brian Clifton <brian@clifton.me>
2020-10-15 22:40:00 -0700

    Revert "templating demo; might help clean up the implementations / de-dupe the code"
    This reverts commit 697d183fd38781c9ab00e466bd5e9b111361c71c.

--- components/p3a/brave_p3a_utils.cc
+++ components/p3a/brave_p3a_utils.cc
@@ -44,61 +44,7 @@ void MaybeRecordNTPCustomizeUsageP3A(NTPCustomizeUsage usage,
   }
 }
 
-std::vector<std::string> prefs_to_register_;
-
-void RegisterP3ARecordedUsageInt(PrefRegistrySimple* local_state) {
-  // TODO: ..
-  for (auto pref : prefs_to_register_) {
-    LOG(ERROR) << "BSC]] demo] " << pref;
-    //local_state->RegisterIntegerPref(kShieldUsageStatus, -1);
-  }
-}
-
-template <typename T> class P3ARecordedUsageInt {
- public:
-  P3ARecordedUsageInt(T max_value, std::string pref_name,
-      std::string event_name)
-      : enum_max_value_(max_value),
-        pref_name_(pref_name),
-        event_name_(event_name) {
-    DCHECK(std::is_enum<T>::value);
-    prefs_to_register_.push_back(pref_name_);
-  }
-
-  void MaybeRecordUsageIntP3A(T new_value, PrefService* local_state) {
-    // May be null in tests.
-    if (!local_state) {
-      return;
-    }
-    int last_value = local_state->GetInteger(pref_name_.c_str());
-    int new_value_int = static_cast<int>(new_value);
-    if (last_value < new_value_int) {
-      // UMA_HISTOGRAM_ENUMERATION(event_name.c_str(), new_value,
-      //     params.enum_max_value);
-      LOG(ERROR) << "BSC]]" << (int)enum_max_value_;
-      local_state->SetInteger(pref_name_.c_str(), new_value_int);
-    }
-  }
-
- private:
-  T enum_max_value_;
-  std::string pref_name_;
-  std::string event_name_;
-};
-
 void RegisterP3AUtilsPrefs(PrefRegistrySimple* local_state) {
-  // example usage (this is what would go into an implementation)
-  P3ARecordedUsageInt<NTPCustomizeUsage> usage(
-      kCustomizeUsageMax,
-      kNTPCustomizeUsageStatus,
-      "Brave.NTP.CustomizeUsageStatus");
-  usage.MaybeRecordUsageIntP3A(kNeverOpened, NULL);
-
-  //...
-  RegisterP3ARecordedUsageInt(local_state);
-
-
-  //old code
   local_state->RegisterIntegerPref(kShieldUsageStatus, -1);
   local_state->RegisterIntegerPref(kNTPCustomizeUsageStatus, -1);
 }
